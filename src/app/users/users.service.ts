8cf29fce274a5a9c9e956445fb0916a62d956f7e
Unknown <anoque@mail.ru>
2018-09-14 17:46:56 +0700

    change users service

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -4,13 +4,22 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class UsersService {
   user: any;
+  status: any;
   constructor(private httpClient: HttpClient) {}
 
-  setUser(data: any) {
+  setUser(data: any): void {
     this.user = data;
   }
 
   getUser(): any {
     return this.user;
+  }
+
+  setStatus(data: any): void {
+    this.status = data;
+  }
+
+  getStatus(): void {
+    return this.status;
   }
 }

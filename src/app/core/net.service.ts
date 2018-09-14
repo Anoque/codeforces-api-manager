7ace6a5f2f3ab466feb18273993fd8f0a95605ae
Unknown <anoque@mail.ru>
2018-09-14 21:21:10 +0700

    add net response for net service

--- src/app/core/net.service.ts
+++ src/app/core/net.service.ts
@@ -1,21 +1,46 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export class NetResponse {
+  constructor(private success: boolean, private response: any) { }
+
+  isSuccess() {
+    return this.success;
+  }
+
+  getResponse() {
+    return  this.response;
+  }
+
+  getErrorMsg() {
+    return this.response['error_msg'] || this.response;
+  }
+}
 
 @Injectable()
 export class NetService {
+  netPackageDebug = true;
 
-    constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
-    public get(url: string): Observable<any> {
-        // const params = new HttpParams().set('id', '3');
-        // const options = {params: params};
+  public get(url: string): Observable<NetResponse> {
+    return this.httpClient.get(url).pipe(map(response => {
+      if (this.netPackageDebug) {
+        console.log('response: ', response);
+      }
+      return this.parseResponse(response);
+    }));
+  }
 
-        return this.httpClient.get(url);
-    }
+  public post(url: string, data: object) {
+    return this.httpClient.post(url, data);
+  }
 
-    public post(url: string, data: object) {
-        return this.httpClient.post(url, data);
-    }
+  private parseResponse(response: any) {
+    const result: any = response['result'] || response['comment'];
+    return new NetResponse((response['status'] === 'OK'), result);
+  }
 
 }

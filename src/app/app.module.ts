819ff526903e8be38b06ae0e119fe4ef30cdb42a
Unknown <anoque@mail.ru>
2018-09-14 17:49:32 +0700

    add shared module and data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContestComponent } from './contest/contest.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -10,9 +9,9 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { CoreModule } from './core/core.module';
 import { MaterialModule } from './core/material.module';
 import { TopMenuComponent } from './core/top-menu/top-menu.component';
-import { UsersModule } from './users/users.module';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -26,6 +25,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
+    SharedModule,
     BrowserAnimationsModule,
     MaterialModule,
     ReactiveFormsModule
---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { NetService } from '../core/net.service';
+import { BehaviorSubject } from 'rxjs';
+
+export class DataStatus {
+  private _initialized: BehaviorSubject<boolean>;
+  get initialized(): BehaviorSubject<boolean> {
+    return this._initialized;
+  }
+
+  private _loaded: BehaviorSubject<boolean>;
+  get loaded(): BehaviorSubject<boolean> {
+    return this._loaded;
+  }
+
+  constructor() {
+    this._initialized = new BehaviorSubject(false);
+    this._loaded = new BehaviorSubject(null);
+  }
+}
+
+export class ContestList {
+  status: DataStatus;
+  data: any[];
+
+  constructor() {
+    this.status = new DataStatus();
+    this.data = [];
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  contestList: ContestList;
+
+  constructor(private netService: NetService) {
+    this.contestList = new ContestList();
+  }
+
+  getContestList() {
+    this.netService.get('http://codeforces.com/api/contest.list').subscribe(res => {
+      this.contestList.status.initialized.next(true);
+      if (typeof res.status !== 'undefined' && res.status === 'OK') {
+        this.contestList.data = res.result;
+        this.contestList.status.loaded.next(true);
+      } else {
+        this.contestList.status.loaded.next(false);
+      }
+    });
+  }
+}

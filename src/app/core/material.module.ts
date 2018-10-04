169b2b39685a6468326899061d8ad8be7799a44b
Unknown <anoque@mail.ru>
2018-10-04 17:20:18 +0700

    add expansion in material module

--- src/app/core/material.module.ts
+++ src/app/core/material.module.ts
@@ -14,6 +14,7 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
     imports: [
@@ -31,7 +32,8 @@ import { MatListModule } from '@angular/material/list';
       MatBadgeModule,
       MatTabsModule,
       MatPaginatorModule,
-      MatListModule
+      MatListModule,
+      MatExpansionModule
     ],
     exports: [
       MatButtonModule,
@@ -48,7 +50,8 @@ import { MatListModule } from '@angular/material/list';
       MatBadgeModule,
       MatTabsModule,
       MatPaginatorModule,
-      MatListModule
+      MatListModule,
+      MatExpansionModule
     ]
 })
 
